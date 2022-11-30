d0dd342604ca18340f66e812191f369231eb63df
Amit0129 <amitkumarnayak40@gmail.com>
2022-11-30 14:23:30 +0530

    Added: UC1 Creating A Contact in Adress Book with with first and last names, address, city, state, zip, phone number and email...

---
+++ AdressBook/AdressBookBuilder.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdressBook
+{
+    internal class AdressBookBuilder
+    {
+         Dictionary<string, Model> addressBookMap = new Dictionary<string, Model>();
+        //public AdressBookBuilder()
+        //{
+        //    this.addressBookMap = new Dictionary<string, Model>();
+        //}
+
+        public void AddAdressBook(string firstName, string lastName, string address, string city, string state, string zip, string phoneNumber,string eMail)
+        {
+            Model model = new Model(firstName, lastName, address, city, state, zip, phoneNumber, eMail);
+            addressBookMap.Add(firstName, model);
+        }
+        public void DisplayAddressBook()
+        {
+            foreach (var item in addressBookMap)
+            {
+                Console.WriteLine($"Contact details First Name :: {item.Value.firstName} Key Is {item.Key}");
+                Console.WriteLine($"Contact details Last Name :: {item.Value.lastName} Key Is {item.Key}");
+                Console.WriteLine($"Contact details Address :: {item.Value.address} Key Is {item.Key}");
+                Console.WriteLine($"Contact details City :: {item.Value.city} Key Is {item.Key}");
+                Console.WriteLine($"Contact details State :: {item.Value.state} Key Is {item.Key}");
+                Console.WriteLine($"Contact details Zip :: {item.Value.zip} Key Is {item.Key}");
+                Console.WriteLine($"Contact details Phone Number :: {item.Value.phoneNumber} Key Is {item.Key}");
+                Console.WriteLine($"Contact details Phone Number :: {item.Value.eMail} Key Is {item.Key}");
+            }
+        }
+    }
+}
--- AdressBook/Program.cs
+++ AdressBook/Program.cs
@@ -5,6 +5,13 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Address Book Program");
+
+            AdressBookBuilder adressBookBuilder = new AdressBookBuilder();
+            adressBookBuilder.AddAdressBook("Amit", "Kumar", "Odisha", "Dhenkanal", "Odisha", "759023", "7978865380","Amit199@gmail.com");
+            adressBookBuilder.DisplayAddressBook();
+
+            
+
         }
     }
 }
