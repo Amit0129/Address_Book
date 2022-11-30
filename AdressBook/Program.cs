5344479f39cadc483a59cae9b5dc4744ef464012
Amit0129 <amitkumarnayak40@gmail.com>
2022-11-30 17:14:25 +0530

    Added: UC5 Use Console to add person details one at a time

--- AdressBook/Program.cs
+++ AdressBook/Program.cs
@@ -7,12 +7,52 @@
             Console.WriteLine("Welcome to Address Book Program");
 
             AdressBookBuilder adressBookBuilder = new AdressBookBuilder();
-            
-            adressBookBuilder.AddAdressBook("Amit", "Kumar", "Odisha", "Dknl", "Odisha", "759023", "7978861488", "amit@gmail.com");
-            adressBookBuilder.AddAdressBook("Manit", "Kumar", "Odisha", "Dknl", "Odisha", "759023", "7978861488", "amit@gmail.com");
-            adressBookBuilder.AddAdressBook("Mamata", "Kumar", "Odisha", "Dknl", "Odisha", "759023", "7978861488", "amit@gmail.com");
-            adressBookBuilder.AddAdressBook("Mayadhar", "Kumar", "Odisha", "Dknl", "Odisha", "759023", "7978861488", "amit@gmail.com");
+
+            bool check = true;
+            while (check)
+            {
+
+                Console.WriteLine("Enter First Name");
+                string firstName = Console.ReadLine();
+
+                Console.WriteLine("Enter Last Name");
+                string lastName = Console.ReadLine();
+
+                Console.WriteLine("Enter Address");
+                string address = Console.ReadLine();
+
+                Console.WriteLine("Enter City Name");
+                string city = Console.ReadLine();
+
+
+                Console.WriteLine("Enter State Name");
+                string state = Console.ReadLine();
+
+                Console.WriteLine("Enter  Zip number");
+                string zip = Console.ReadLine();
+
+                Console.WriteLine("Enter Phone Number");
+                string pnoneNumber = Console.ReadLine();
+
+                Console.WriteLine("Enter Email");
+                string email = Console.ReadLine();
+
+                adressBookBuilder.AddAdressBook(firstName, lastName, address, city, state, zip, pnoneNumber, email);
+
+                Console.WriteLine("If you want to add another Enter Y ");
+                string addOrNot = Console.ReadLine().ToLower();
+
+                if (addOrNot == "y")
+                {
+                    check = true;
+                }
+                else
+                {
+                    check = false;
+                }
+            }
             adressBookBuilder.DisplayAddressBook();
+
 
             Console.WriteLine("Choose : \n1) To Edit A Contact Details\n2) To Delete A Contact Details");
             int choice = Convert.ToInt32(Console.ReadLine());
@@ -24,14 +64,14 @@
                     Console.WriteLine("Enter a person name u want to Edit Details");
                     string editName = Console.ReadLine();
                     adressBookBuilder.EditContact(editName);
-                    adressBookBuilder.DisplayAddressBook();
+                    adressBookBuilder.DisplayAddressBook();   //To Check Editing Work Or not
                     break;
                 case 2:
                     //For Deleate A Person Contact Details
                     Console.WriteLine("Enter a person name u want to remove");
                     string name = Console.ReadLine();
                     adressBookBuilder.RemoveContact(name);
-                    adressBookBuilder.DisplayAddressBook();
+                    //adressBookBuilder.DisplayAddressBook();   //To check Detetation Work Or not
                     break;
                 default:
                     Console.WriteLine("Please Choose 1 for Edit Or 2 for Delete");
